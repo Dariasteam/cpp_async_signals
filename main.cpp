0935b60c9fb124efe9dc5f392ef8ae15ebdb0c44
dariasteam <dariasteam94@gmail.com>
2018-03-21 19:41:10 +0000

    Uso de colas en vez de vectores

--- main.cpp
+++ main.cpp
@@ -12,24 +12,31 @@ using namespace std;
 
 bool end_program;
 long long unsigned counter;
-std::vector<std::function<void (unsigned)>> asynchronous_functions;
+std::queue<std::function<void (unsigned)>> asynchronous_functions;
 unsigned id;
+std::queue<future<void>> promises;
 
-void asynchronous_dispatcher () {
-  std::vector<future<void>> promises;
-  unsigned last_index = 0;
-  std::cout << "Dispatcher iniciado" << std::flush;
+// Encargado de recoger las funciones que deben realizarse y alimentar al dispatcher
+void asynchronous_handler () {
   while (!end_program) {
-    for (unsigned i = last_index; i < asynchronous_functions.size(); i++) {
-      promises.push_back (std::async(asynchronous_functions[i], id));
-      last_index++;
-    }
-    for (unsigned i = last_index; i < promises.size(); i++) {
-      promises[i].get();
+    while (!asynchronous_functions.empty()) {
+      promises.push (std::async(asynchronous_functions.front(), id));
+      asynchronous_functions.pop();
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
-  std::cout << "El dispatcher ha terminado\n" << std::flush;
+}
+
+// Encargado de realizar las funciones de forma asíncrona
+void asynchronous_dispatcher () {
+  unsigned last_index = 0;
+  while (!end_program) {
+    while (!promises.empty()) {
+      promises.front().get();
+      promises.pop();
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
 }
 
 void handle_response_0 (unsigned) {
@@ -49,23 +56,35 @@ void handle_response_1 (unsigned i) {
   handle_response_2(i);
 }
 
-void handle_response_3 (unsigned) {
+void handle_response_3 (unsigned i) {
+  std::vector <std::future<void>> promises (10);
+  for (auto& promise : promises)
+    promise = std::async(handle_response_1, i);
+
+  for (auto& promise : promises)
+    promise.get();
+}
+
+void handle_response_4 (unsigned) {
   end_program = true;
 }
 
 void master_handler (unsigned option) {
   switch (option) {
     case 0:
-      asynchronous_functions.push_back(&handle_response_0);
+      asynchronous_functions.push(&handle_response_0);
       break;
     case 1:
-      asynchronous_functions.push_back(&handle_response_1);
+      asynchronous_functions.push(&handle_response_1);
       break;
     case 2:
-      asynchronous_functions.push_back(&handle_response_2);
+      asynchronous_functions.push(&handle_response_2);
       break;
     case 3:
-      asynchronous_functions.push_back(&handle_response_3);
+      asynchronous_functions.push(&handle_response_3);
+      break;
+    case 4:
+      asynchronous_functions.push(&handle_response_4);
       break;
   }
 }
@@ -74,9 +93,10 @@ void wait_for_user_input () {
   unsigned user_input;
   while (!end_program) {
     std::cout << "[0] Ver el estado de la operación en segundo plano" << "\n"
-              << "[1] Realizar algoritmo 'costoso' secuencialmente, A+B"<< "\n"
-              << "[2] Realizar algoritmo 'costoso' secuencialmente, solo B"<< "\n"
-              << "[3] Salir"<< "\n";
+              << "[1] Realizar algoritmo 'costoso', A+B"<< "\n"
+              << "[2] Realizar algoritmo 'costoso', solo B"<< "\n"
+              << "[3] Realizar 10 instancias del algoritmo 'costoso' en paralelo"<< "\n"
+              << "[4] Salir"<< "\n";
     std::cin >> user_input;
     master_handler (user_input);
     id++;
@@ -93,7 +113,7 @@ void do_stuff_in_background () {
     }
     std::cout << "La operación en segundo plano ha terminado\n" << std::flush;
   };
-  asynchronous_functions.push_back(f);
+  asynchronous_functions.push(f);
 }
 
 int main() {
@@ -101,11 +121,13 @@ int main() {
   counter = 0;
   id = 0;
 
-  std::vector<std::future<void>> promises (2);
+  std::vector<std::future<void>> promises (3);
 
   promises[0] = std::async(wait_for_user_input);
-  do_stuff_in_background();
   promises[1] = std::async(asynchronous_dispatcher);
+  promises[2] = std::async(asynchronous_handler);
+
+  do_stuff_in_background();
 
   for (auto& p : promises) {
     p.wait();
