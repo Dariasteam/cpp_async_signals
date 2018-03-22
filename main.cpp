c6e68a560fc60dd23b3755fc6f1f7fe8f0c9bead
dariasteam <dariasteam94@gmail.com>
2018-03-22 00:49:28 +0000

    Eliminando static

--- main.cpp
+++ main.cpp
@@ -12,14 +12,12 @@ long long unsigned counter;
 
 class AsyncManager {
 private:
-  std::queue<std::function<void (unsigned)>> asynchronous_functions;
+  std::queue<std::function<void (unsigned, AsyncManager* const manager)>> asynchronous_functions;
   unsigned id;
   std::queue<std::future<void>> asynchronous_promises;
 public:
-  static bool end_program;
-  AsyncManager() {
-    AsyncManager::end_program = false;
-  }
+  bool end_program;
+  AsyncManager() : end_program(false) {}
 
   void execute () {
     std::vector<std::future<void>> promises (2);
@@ -28,7 +26,7 @@ public:
     promises[0] = std::async([&]() {
       while (!end_program) {
         while (!asynchronous_functions.empty()) {
-          asynchronous_promises.push (std::async(asynchronous_functions.front(), id));
+          asynchronous_promises.push (std::async(asynchronous_functions.front(), id, this));
           asynchronous_functions.pop();
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
@@ -42,7 +40,7 @@ public:
           asynchronous_promises.front().wait();
           asynchronous_promises.pop();
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
       }
     });
 
@@ -51,110 +49,105 @@ public:
     }
   }
 
-  void add_function (std::function<void (unsigned)> function) {
+  void add_function (std::function<void (unsigned, AsyncManager* const manager)> function) {
     id++;
     asynchronous_functions.push(function);
   }
 
-  void add_persistent_function (std::function<void (unsigned)> function) {
-    auto persistent = [&](unsigned i) {
+  void add_persistent_function (std::function<void (unsigned, AsyncManager* const manager)> function) {
+    auto persistent = [&](unsigned i, AsyncManager* const manager) {
       while (!AsyncManager::end_program) {
-        function (i);
+        function (i, manager);
       }
     };
     add_function(persistent);
   }
 
-  static void end_process () {
+  void end_process () {
     end_program = true;
   }
 };
 
-bool AsyncManager::end_program = false;
-AsyncManager manager;
-
-void handle_response_0 (unsigned) {
+void handle_response_0 (unsigned, AsyncManager* const manager) {
   std::cout << "\t" << "El contador en segundo plano marca " << counter << "\n" << std::flush;
 }
 
-void handle_response_2 (unsigned i) {
+void handle_response_2 (unsigned i, AsyncManager* const manager) {
   std::cout << "\t" << "Comenzando B " << i << "\n" << std::flush;
   std::this_thread::sleep_for(std::chrono::milliseconds(3000));
   std::cout << "\t" << "B finalizado " << i << "\n" << std::flush;
 }
 
-void handle_response_1 (unsigned i) {
+void handle_response_1 (unsigned i, AsyncManager* const manager) {
   std::cout << "\t" << "Comenzando A " << i << "\n" << std::flush;
   std::this_thread::sleep_for(std::chrono::milliseconds(3000));
   std::cout << "\t" << "A finalizado " << i << "\n" << std::flush;
-  handle_response_2(i);
+  handle_response_2(i, manager);
 }
 
-void handle_response_3 (unsigned i) {
+void handle_response_3 (unsigned i, AsyncManager* const manager) {
   std::vector <std::future<void>> promises (10);
   for (auto& promise : promises)
-    promise = std::async(handle_response_1, i);
+    promise = std::async(handle_response_1, i, manager);
 
   for (auto& promise : promises)
     promise.get();
 }
 
-void handle_response_4 (unsigned) {
-  AsyncManager::end_process();
+void handle_response_4 (AsyncManager* const manager) {
+  manager->end_process();
 }
 
-void master_handler (unsigned option) {
+void master_handler (unsigned option, AsyncManager* const manager) {
   switch (option) {
     case 0:
-      manager.add_function(&handle_response_0);
+      manager->add_function(&handle_response_0);
       break;
     case 1:
-      manager.add_function(&handle_response_1);
+      manager->add_function(&handle_response_1);
       break;
     case 2:
-      manager.add_function(&handle_response_2);
+      manager->add_function(&handle_response_2);
       break;
     case 3:
-      manager.add_function(&handle_response_3);
+      manager->add_function(&handle_response_3);
       break;
     case 4:
-      manager.add_function(&handle_response_4);
+      handle_response_4(manager);
       break;
   }
 }
 
-void wait_for_user_input (unsigned i) {
+void wait_for_user_input (unsigned i, AsyncManager* const manager) {
   unsigned user_input;
-  std::cout << "[0] Ver el estado de la operación en segundo plano" << i << "\n"
+  std::cout << "[0] Ver el estado de la operación en segundo plano" << "\n"
             << "[1] Realizar algoritmo 'costoso', A+B"<< "\n"
             << "[2] Realizar algoritmo 'costoso', solo B"<< "\n"
             << "[3] Realizar 10 instancias del algoritmo 'costoso' en paralelo"<< "\n"
             << "[4] Salir"<< "\n";
   std::cin >> user_input;
-  master_handler (user_input);
+  master_handler (user_input, manager);
 }
 
-void do_stuff_in_background (unsigned i) {
+void do_stuff_in_background (unsigned i, AsyncManager* const manager) {
   counter++;
 }
 
-void generate_logs (unsigned) {
+void generate_logs (unsigned, AsyncManager* const manager) {
   std::ofstream outfile;
   outfile.open("test.txt", std::ios_base::app);
   outfile << "log : " << counter << "\n";
-  std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 }
 
 int main() {
+  AsyncManager manager;
   counter = 0;
 
   manager.add_persistent_function(&wait_for_user_input);
   manager.add_persistent_function(&do_stuff_in_background);
   manager.add_persistent_function(&generate_logs);
   manager.execute();
-
-  std::cout << "Programa finalizado" << std::endl;
-  return 0;
 }
 
 
